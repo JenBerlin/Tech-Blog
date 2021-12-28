02ebec0ee3a8694b4003a9ee788e97649776064f
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-12-28 16:31:05 +0100

    Creat models index.js

--- controllers/home_routes.js
+++ controllers/home_routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
+const models = require("../models");
 
 router.get("/", async (req, res) => {
+  const posts = models.Post.findAll();
+  // To be finished
   res.render("home");
 });
 
