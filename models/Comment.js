abc55a6b7d3e05523dab7516a06c0cee54c0962a
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-12-28 16:12:18 +0100

    Creat models post.js comment.js

---
+++ models/Comment.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: {
+        args: [20, 200],
+        msg: "Please provide field within 20 to 200 characters.",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
