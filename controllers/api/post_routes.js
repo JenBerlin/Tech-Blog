daadc93bc7cfcbf25df4095f9a27ee015d11247e
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2022-01-03 21:41:11 +0100

    Implement delete post function

--- controllers/api/post_routes.js
+++ controllers/api/post_routes.js
@@ -21,4 +21,25 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Delete a post from database (dashboard).
+// "/:id" because of the : I'm transforming information directly to the database
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletePost = await Post.destroy({
+      where: {
+        // params = "/:id"
+        id: req.params.id,
+      },
+    });
+    if (!deletePost) {
+      return res.status(404).json({ error: "Failed to delete the post." });
+    }
+    return res.status(200).json({ data: "It has been delete." });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Failed to delete the post." });
+  }
+  // localhost:3001/api/posts/:id
+});
+
 module.exports = router;
